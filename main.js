404558b6e19ec48826bd8dc771484c3b906b5bfe
Daisuke-Matsubara <110824363+Daisuke-Matsubara@users.noreply.github.com>
2023-02-18 19:08:38 +0900

    R5.2.18

---
+++ main.js
@@ -0,0 +1,10 @@
+let box = document.querySelector('.box');
+console.log(box);
+
+let box1 = document.querySelector('#box');
+console.log(box1);
+
+let boxClass = document.querySelectorAll('.box');
+for(let i = 0;i < boxClass.length;i++) {
+    console.log(boxClass);
+}
